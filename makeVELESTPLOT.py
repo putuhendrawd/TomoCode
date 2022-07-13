b57c5bb2f7423cca33cfed655fdef9bef23e78ee
putuhendrawd <99854901+putuhendrawd@users.noreply.github.com>
2022-07-13 13:38:36 +0700

    make output fig

--- makeVELESTPLOT.py
+++ makeVELESTPLOT.py
@@ -2,15 +2,27 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter, AutoMinorLocator
+from matplotlib.lines import Line2D
 from localfunction import *
 
+path = "E:\\My Drive\\Tomography\\130722\\"
 # input from mod
 
 
 #manual input 
-depth = [-5,0,10,20,30,40,50,60,70,90,120,140,210,360,510]
-vp = [6.781,6.810,6.867,6.924,6.981,7.038,7.095,7.152,7.209,7.323,7.494,7.608,8.007,8.862,9.717]
-#vp = [4.6,6.20,6.20,6.21,6.66,7.54,7.67,7.67,8.14,8.14,8.16,8.19,8.39,8.94,9.03]
+depth = [-10.0,0.0,5.0,10.0,20.0,30.0,40.0,60.0]
+vp = [[6.75,6.8,6.83,6.86,6.92,6.98,7.03,7.15],
+      [6.75,6.79,6.82,6.85,6.96,7.05,7.14],
+      [6.75,6.79,6.81,6.84,6.96,7.08,7.19],
+      [6.75,6.79,6.81,6.83,6.95,7.11,7.22],
+      [6.75,6.79,6.81,6.83,6.95,7.11,7.24],
+      [6.75,6.79,6.81,6.83,6.94,7.11,7.25],
+      [6.75,6.79,6.81,6.83,6.94,7.1,7.26],
+      [6.75,6.78,6.81,6.82,6.94,7.1,7.26],
+      [6.75,6.78,6.81,6.82,6.94,7.09,7.26],
+      [6.75,6.78,6.81,6.82,6.94,7.09,7.26],
+      [6.75,6.78,6.81,6.82,6.94,7.09,7.26,8.27]
+      ]
 
 #=========================================
 def makeinput(depth,vp):
@@ -24,14 +36,35 @@ def makeinput(depth,vp):
     return(depth2,vp2)
 #=========================================
 
-depth2,vp2 = makeinput(depth,vp)
-
 fig,ax = plt.subplots(figsize=(3,6), dpi = 300)
-ax.plot(vp2,depth2)
-ax.set_ylim([-10,max(depth2)-10])
+
+#plot init
+depth2,vp2 = makeinput(depth,vp[0])
+ax.plot(vp2,depth2,color='blue',linestyle='--')
+
+#plot iter
+for i in range(1,len(vp)-1):
+    depth2,vp2 = makeinput(depth[:7],vp[i])
+    ax.plot(vp2,depth2,color='grey')
+
+#plot output
+depth2,vp2 = makeinput(depth,vp[-1])
+ax.plot(vp2,depth2,color='blue')
+
+#make legend
+custom_lines = [Line2D([0], [0], color='blue', linestyle='--', lw=1),
+                Line2D([0], [0], color='grey', lw=1),
+                Line2D([0], [0], color='blue', lw=1)]
+ax.legend(custom_lines, ['Input Model', 'Interation', 'Updated Model'],prop={'size': 5})
+
+ax.set_xlim([6.7,7.3])
+ax.set_ylim([-10,max(depth)])
 ax.xaxis.set_minor_locator(AutoMinorLocator())
+ax.xaxis.set_tick_params(top=True, direction='in',which = 'both')
 ax.yaxis.set_minor_locator(AutoMinorLocator())
+ax.yaxis.set_tick_params(right=True, direction='in', which = 'both')
 ax.invert_yaxis()
 ax.set_title('1-D Model')
 ax.set_xlabel('P-Wave Velocity [km/s]')
 ax.set_ylabel('Depth [km]')
+fig.savefig(path+"output_velest.jpg",bbox_inches = 'tight')
