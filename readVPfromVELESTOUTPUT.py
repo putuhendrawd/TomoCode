a7f23ff283f821886f65f81bf083e4e813bf8df0
putuhendrawd <99854901+putuhendrawd@users.noreply.github.com>
2022-07-13 21:44:50 +0700

    auto read from MODparam

--- readVPfromVELESTOUTPUT.py
+++ readVPfromVELESTOUTPUT.py
@@ -1,16 +1,39 @@
+'''
+Coding: PYTHON UTF-8
+Created On: 2022-07-13 21:29:05
+Author: Putu Hendra Widyadharma
+=== reading velest output .OUT || need input depth and initial vp from readMODparam function
+'''
 import pandas as pd
 from localfunction import *
 
 path = "E:\\My Drive\\Tomography\\130722\\Velest33-indoburma\\"
 fname = "VELEST_OUT.OUT"
 
-fin = open(path+fname)
+#input parameter
+fin = open(path+'inputreformat\\MODparam.txt')
 readl = fin.readlines()
+#depth
+depth = readl[2].split(",")
+depth = ' '.join(depth).split()
+depth = depth[1::]
+depth = [float(i) for i in depth]
+#initial vp
+initial = readl[3].split(",")
+initial = ' '.join(initial).split()
+initial = initial[1::]
+initial = [float(i) for i in initial]
+fin.close()
 
-depth = [-5.0,0.0,10.0,20.0,40.0,50.0,60.0,70.0,90.0,120.0,140.0,165.0,210.0,360.0,510.0]
-result = []
+result = pd.DataFrame([],columns=["depth","init"])
+result["depth"] = depth
+result['init'] = initial
+result.set_index("depth",inplace=True)
 start = False
 
+#start
+fin = open(path+fname)
+readl = fin.readlines()
 for i in range(len(readl)):
     spl = readl[i].split()
     
@@ -18,27 +41,27 @@ for i in range(len(readl)):
         start = True
     
     if len(spl) == 3 and spl[0] == "ITERATION" and start:
-        result.append([spl[0],spl[2]])
+        iter = spl[2]
     try:
         if len(spl) == 3 and (float(spl[2]) in depth) and start:
-            result.append([spl[2],spl[0]])
+            result.loc[float(spl[2]),iter] = spl[0]
     except:
         continue
     
     if len(spl) == 12 and spl[0] == "nlay":
         start = 2
-        result.append(["LAST","RESULT"])
+        iter = 0
     
     try:   
         if (float(spl[1].split('.')[0]) in depth) and start == 2:
             if len(readl[i].split()) == 13:
-                result.append([spl[1].split('.')[0],spl[4]])
+                result.loc[float(spl[1].split('.')[0]),iter] = spl[4]
+                # result.append([spl[1].split('.')[0],spl[4]])
             else: 
-                result.append([spl[1].split('.')[0],spl[3]])
+                result.loc[float(spl[1].split('.')[0]),iter] = spl[3]
+                # result.append([spl[1].split('.')[0],spl[3]])
     except:
         continue
     
 #export
-
-dfresult = pd.DataFrame(result)
-dfresult.to_csv(path+'vp_extract.txt',header=False,index=False)
+result.to_csv(path+'\\output\\vp_extract.txt',index="depth")
