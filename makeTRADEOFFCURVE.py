7ed7a04e37531945c24c886542e1a076c09ce474
putuhendrawd <putuhendrawd@gmail.com>
2022-08-01 08:09:51 +0700

    commit

---
+++ makeTRADEOFFCURVE.py
@@ -0,0 +1,14 @@
+'''
+Coding: PYTHON UTF-8
+Created On: 2022-08-01 08:02:19
+Author: Putu Hendra Widyadharma
+=== make trade off curve model variance vs data variance
+'''
+
+import numpy as np
+import pandas as pd
+import geopy.distance as gd
+import matplotlib.pyplot as plt
+from numpy import float64, math
+from localfunction import *
+pd.options.mode.chained_assignment = None
