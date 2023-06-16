08b6b860677a096b4131781c3d91d005dcb1569a
putuhendrawd <99854901+putuhendrawd@users.noreply.github.com>
2023-06-16 14:56:42 +0700

    adding output type --> pha

--- convertARRIVAL.py
+++ convertARRIVAL.py
@@ -19,62 +19,87 @@ def cdate(param):
 def ctime(param):
     return(dt.strptime(param,"%H:%M:%S.%f"))
     
-    
 # import path and data
 path = "arrival/"
-arrival_fname = "arrivals.txt"
+arrival_fname = "arrivals_demo.txt"
 station_fname = "etc/indonesia_station.txt"
+output_type = "pha" #stead or pha
 
 # check if output files with name exist:
-if os.path.isfile(path+Path(arrival_fname).stem+".csv"):
+if os.path.isfile(path+Path(arrival_fname).stem+".csv") and output_type == "stead":
     print(f"output file exist! \nplease delete {path+Path(arrival_fname).stem+'.csv'}")
+    exit()
+elif os.path.isfile(path+Path(arrival_fname).stem+".pha") and output_type == "pha":
+    print(f"output file exist! \nplease delete {path+Path(arrival_fname).stem+'.pha'}")
     exit()
 
 # read station
 sta_dict = pd.read_csv(station_fname, sep="\t",names=['station','lat','lon','elevation']).set_index('station').to_dict('index')
 
 # define dataframe columns for STEAD format
-# adding s_travel_sec 
-df = pd.DataFrame(columns= [
-            'network_code',
-            'receiver_code',
-            'receiver_type',
-            'receiver_latitude',
-            'receiver_longitude',
-            'receiver_elevation_m',
-            'p_arrival_sample',
-            'p_status',
-            'p_weight',
-            'p_travel_sec',
-            's_arrival_sample',
-            's_status',
-            's_weight',
-            's_travel_sec',
-            'source_id',
-            'date_',
-            'source_origin_time',
-            'source_origin_uncertainty_sec',
-            'source_latitude',
-            'source_longitude',
-            'source_error_sec',
-            'source_gap_deg',
-            'source_horizontal_uncertainty_km',
-            'source_depth_km',
-            'source_depth_uncertainty_km',
-            'source_magnitude',
-            'source_magnitude_type',
-            'source_magnitude_author',
-            'source_mechanism_strike_dip_rake',
-            'source_distance_deg',
-            'source_distance_km',
-            'back_azimuth_deg',
-            'snr_db','coda_end_sample',
-            'trace_start_time',
-            'trace_category',
-            'trace_name'])
-
-df.to_csv(path+Path(arrival_fname).stem+".csv",mode='a', header=True, index=False)
-
+if output_type == "stead":
+    # adding s_travel_sec 
+    df = pd.DataFrame(columns= [
+                'network_code',
+                'receiver_code',
+                'receiver_type',
+                'receiver_latitude',
+                'receiver_longitude',
+                'receiver_elevation_m',
+                'p_arrival_sample',
+                'p_status',
+                'p_weight',
+                'p_travel_sec',
+                's_arrival_sample',
+                's_status',
+                's_weight',
+                's_travel_sec',
+                'source_id',
+                'date_',
+                'source_origin_time',
+                'source_origin_uncertainty_sec',
+                'source_latitude',
+                'source_longitude',
+                'source_error_sec',
+                'source_gap_deg',
+                'source_horizontal_uncertainty_km',
+                'source_depth_km',
+                'source_depth_uncertainty_km',
+                'source_magnitude',
+                'source_magnitude_type',
+                'source_magnitude_author',
+                'source_mechanism_strike_dip_rake',
+                'source_distance_deg',
+                'source_distance_km',
+                'back_azimuth_deg',
+                'snr_db',
+                'coda_end_sample',
+                'trace_start_time',
+                'trace_category',
+                'trace_name'])
+    df.to_csv(path+Path(arrival_fname).stem+".csv",mode='a', header=True, index=False)
+# define outputfile for pha format
+elif output_type == "pha":
+    df = pd.DataFrame(columns= [
+                    'flag',
+                    'year',
+                    'month',
+                    'day',
+                    'hour',
+                    'minutes',
+                    'seconds',
+                    'latitude',
+                    'longitude',
+                    'depth',
+                    'magnitude',
+                    'horizontal_error',
+                    'depth_error',
+                    'RMS',
+                    'ID',])
+else:
+    print(f"output file format not supported --> {output_type}")
+    exit()
+    
 # start read and convert
 # initiate trigger bool and trigger counter
 trigger_d = {
@@ -85,6 +110,7 @@ trigger_d = {
     "stamag" : False,
     }
 trigger_counter = 0
+event_counter = 1
 
 # run
 fin = open(path+arrival_fname,'r')
@@ -158,9 +184,29 @@ for i, line in enumerate(lines):
             source_mag_ = line.split()[1]
             source_mag_type_ = line.split()[0]
     
-    # phase arrival magnitude counter 
+    # phase arrival block reading + writing output format
     if trigger_d['phase']:
-        if len(line.split()) > 3 and "sta" not in line.lower():
+        if len(line.split()) > 3 and "sta" in line.lower() and output_type == "pha":
+            phaheader_ = {
+                    'flag'                 :   ["#"], ### done ###
+                    'year'                 :   [date_.year], ### done ###
+                    'month'                :   [date_.month], ### done ###
+                    'day'                  :   [date_.day], ### done ###
+                    'hour'                 :   [time_.hour], ### done ###
+                    'minutes'              :   [time_.minute], ### done ###
+                    'seconds'              :   [time_.strftime("%S.%f")], ### done ###
+                    'latitude'             :   [lat_], ### done ###
+                    'longitude'            :   [lon_], ### done ###
+                    'depth'                :   [depth_], ### done ###
+                    'magnitude'            :   [source_mag_], ### done ###
+                    'horizontal_error'     :   [source_horizontal_uncertainty_km_], ### done ###
+                    'depth_error'          :   [depth_uncertainty_km_], ### done ###
+                    'RMS'                  :   [source_error_sec_], ### done ###
+                    'ID'                   :   [event_counter], ### done ###
+                    }
+            df = pd.concat([df,pd.DataFrame(phaheader_)])
+            
+        elif len(line.split()) > 3 and "sta" not in line.lower():
             # print(line)
             net_ = line.split()[1]
             sta_ = line.split()[0]
@@ -190,6 +236,7 @@ for i, line in enumerate(lines):
                 s_weight_ = None
                 s_travel_sec_ = None
                 s_status_ = None
+                tt_pha = p_travel_sec_
             elif pha == 'S':
                 p_arrival_sample_ = None
                 p_weight_ = None
@@ -199,6 +246,7 @@ for i, line in enumerate(lines):
                 s_weight_ = line.split()[-2]
                 s_travel_sec_ = abs(ctime(s_arrival_sample_) - time_).total_seconds()
                 s_status_ = mode_
+                tt_pha = s_travel_sec_
             else:
                 p_arrival_sample_ = None
                 p_weight_ = None
@@ -208,55 +256,90 @@ for i, line in enumerate(lines):
                 s_weight_ = None
                 s_travel_sec_ = None
                 s_status_ = None
+                tt_pha = None
             
-            # save data as dataframe
-            data_ = {
-                'network_code'                  :   [net_], ### done ###
-                'receiver_code'                 :   [sta_], ### done ###
-                'receiver_type'                 :   [receiver_type_], ### done ###
-                'receiver_latitude'             :   [lat_sta_], ### done ###
-                'receiver_longitude'            :   [lon_sta_], ### done ###
-                'receiver_elevation_m'          :   [elev_sta_], ### done ###
-                'p_arrival_sample'              :   [p_arrival_sample_], ### done ###
-                'p_status'                      :   [p_status_], ### done ###
-                'p_weight'                      :   [p_weight_], ### done ###
-                'p_travel_sec'                  :   [p_travel_sec_], ### done ###
-                's_arrival_sample'              :   [s_arrival_sample_], ### done ###
-                's_status'                      :   [s_status_], ### done ###
-                's_weight'                      :   [s_weight_], ### done ###
-                's_travel_sec'                  :   [p_travel_sec_], ### done ###
-                'source_id'                     :   [eventid_], ### done ###
-                'date_'                         :   [date_.strftime("%d/%m/%Y")], ### done ###
-                'source_origin_time'            :   [datetime_.strftime("%d/%m/%Y %H:%M:%S.%f")], ### done ###
-                'source_origin_uncertainty_sec' :   [time_uncertainty_sec_], ### done ###
-                'source_latitude'               :   [lat_], ### done ###
-                'source_longitude'              :   [lon_], ### done ###
-                'source_error_sec'              :   [source_error_sec_], ### done ###
-                'source_gap_deg'                :   [azgap_], ### done ###
-                'source_horizontal_uncertainty_km': [source_horizontal_uncertainty_km_], ### done ###
-                'source_depth_km'               :   [depth_], ### done ###
-                'source_depth_uncertainty_km'   :   [depth_uncertainty_km_], ### done ###
-                'source_magnitude'              :   [source_mag_], ### done ###
-                'source_magnitude_type'         :   [source_mag_type_], ### done ###
-                'source_magnitude_author'       :   [author_], ### done ###
-                'source_mechanism_strike_dip_rake': ['None'],
-                'source_distance_deg'           :   [source_dist_deg_],
-                'source_distance_km'            :   [source_dist_km_],
-                'back_azimuth_deg'              :   [azimuth_],
-                'snr_db'                        :   ['None'],
-                'coda_end_sample'               :   ['None'],
-                'trace_start_time'              :   ['None'],
-                'trace_category'                :   ['None'],
-                'trace_name'                    :   ['None']
-                }
-            df = pd.concat([df,pd.DataFrame(data_)])
+            # save data stead
+            if output_type == "stead":
+                data_ = {
+                    'network_code'                  :   [net_], ### done ###
+                    'receiver_code'                 :   [sta_], ### done ###
+                    'receiver_type'                 :   [receiver_type_], ### done ###
+                    'receiver_latitude'             :   [lat_sta_], ### done ###
+                    'receiver_longitude'            :   [lon_sta_], ### done ###
+                    'receiver_elevation_m'          :   [elev_sta_], ### done ###
+                    'p_arrival_sample'              :   [p_arrival_sample_], ### done ###
+                    'p_status'                      :   [p_status_], ### done ###
+                    'p_weight'                      :   [p_weight_], ### done ###
+                    'p_travel_sec'                  :   [p_travel_sec_], ### done ###
+                    's_arrival_sample'              :   [s_arrival_sample_], ### done ###
+                    's_status'                      :   [s_status_], ### done ###
+                    's_weight'                      :   [s_weight_], ### done ###
+                    's_travel_sec'                  :   [p_travel_sec_], ### done ###
+                    'source_id'                     :   [eventid_], ### done ###
+                    'date_'                         :   [date_.strftime("%d/%m/%Y")], ### done ###
+                    'source_origin_time'            :   [datetime_.strftime("%d/%m/%Y %H:%M:%S.%f")], ### done ###
+                    'source_origin_uncertainty_sec' :   [time_uncertainty_sec_], ### done ###
+                    'source_latitude'               :   [lat_], ### done ###
+                    'source_longitude'              :   [lon_], ### done ###
+                    'source_error_sec'              :   [source_error_sec_], ### done ###
+                    'source_gap_deg'                :   [azgap_], ### done ###
+                    'source_horizontal_uncertainty_km': [source_horizontal_uncertainty_km_], ### done ###
+                    'source_depth_km'               :   [depth_], ### done ###
+                    'source_depth_uncertainty_km'   :   [depth_uncertainty_km_], ### done ###
+                    'source_magnitude'              :   [source_mag_], ### done ###
+                    'source_magnitude_type'         :   [source_mag_type_], ### done ###
+                    'source_magnitude_author'       :   [author_], ### done ###
+                    'source_mechanism_strike_dip_rake': ['None'],
+                    'source_distance_deg'           :   [source_dist_deg_],
+                    'source_distance_km'            :   [source_dist_km_],
+                    'back_azimuth_deg'              :   [azimuth_],
+                    'snr_db'                        :   ['None'],
+                    'coda_end_sample'               :   ['None'],
+                    'trace_start_time'              :   ['None'],
+                    'trace_category'                :   ['None'],
+                    'trace_name'                    :   ['None']
+                    }
+                df = pd.concat([df,pd.DataFrame(data_)])
+            # save data pha  
+            elif output_type == "pha":
+                phadata_ = {
+                    'flag'                 :   [sta_], ### changing data to STA ###
+                    'year'                 :   [tt_pha], ### changing data to TT ###
+                    'month'                :   ['1'], ### changing data to WEIGHT ### #default falue#
+                    'day'                  :   [pha], ### changing data to PHA TYPE ###
+                    'hour'                 :   [None], ### changing data to None After this ###
+                    'minutes'              :   [None], 
+                    'seconds'              :   [None], 
+                    'latitude'             :   [None], 
+                    'longitude'            :   [None], 
+                    'depth'                :   [None],
+                    'magnitude'            :   [None],
+                    'horizontal_error'     :   [None], 
+                    'depth_error'          :   [None], 
+                    'RMS'                  :   [None], 
+                    'ID'                   :   [None], 
+                    }
+                df = pd.concat([df,pd.DataFrame(phadata_)])
+        else:
+            pass
         
     # data saving
     if trigger_counter == 5:
-        #save data
-        df = df.replace('',np.nan).groupby('receiver_code', as_index=False).first().fillna('')
-        df.to_csv(path+Path(arrival_fname).stem+".csv",mode='a', header=False, index=False)
-        # pruge df
-        df = df[0:0]
+        #save data stead
+        if output_type == "stead":
+            df = df.replace('',np.nan).groupby('receiver_code', as_index=False).first().fillna('')
+            df.to_csv(path+Path(arrival_fname).stem+".csv",mode='a', header=False, index=False)
+            # pruge df
+            df = df[0:0]
+        #save data pha
+        else:
+            df.reset_index(inplace = True, drop = True)
+            df2dat(df,evnum = 0, path = path, fname=Path(arrival_fname).stem+'.pha', mode = 'a', verbose=False)
+            # pruge df
+            df = df[0:0]
+            pass    
         print(f"processing {eventid_}")
+        event_counter += 1
         trigger_counter = 0
+
+print("convert finish!")
