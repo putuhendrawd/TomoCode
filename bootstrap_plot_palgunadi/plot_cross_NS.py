32b80b5c037a21a5e8a3ef170238dd6126f391a1
putuhendrawd <99854901+putuhendrawd@users.noreply.github.com>
2023-08-08 14:41:45 +0700

    new!

---
+++ bootstrap_plot_palgunadi/plot_cross_NS.py
@@ -0,0 +1,102 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+from mpl_toolkits.basemap import Basemap
+
+N = 50 #Number of Realization/Jumlah bootstrap sample
+M = 26000 #>=ID event terakhir/Jumlah event
+hypo = np.loadtxt('tomo.reloc', usecols=(0,1,2,3))
+id = np.array([int(l)-1 for l in hypo[:,0]])
+hx = hypo[:,2]
+hy = hypo[:,1]
+hz = hypo[:,3]
+
+# initialize matrix
+X = np.zeros((M,N))
+Y = np.zeros((M,N))
+Z = np.zeros((M,N))
+
+for i in range(N):
+    data = np.loadtxt('tomo'+str(i)+'.reloc', usecols=(0,1,2,3))
+    idx = np.array([int(l)-1 for l in data[:,0]])
+    X[idx,i] = data[:,2]
+    Y[idx,i] = data[:,1]
+    Z[idx,i] = data[:,3]
+
+X[X==0]=9999
+Y[Y==0]=9999
+Z[Z==0]=9999
+
+
+## MapView
+lon_min=137.105
+lon_max=137.118
+lat_min=-4.0643
+lat_max=-4.054
+interval=0.002
+dep_min=-2.0
+dep_max=-3.2
+fig = plt.gcf()
+ax = fig.add_subplot(111, aspect='auto')
+ax.set_xlim(lat_min, lat_max)
+ax.set_ylim(dep_min, dep_max)
+
+ll = 0
+for mm in id:
+    x = X[mm,:]
+    y = Y[mm,:]
+    z = Z[mm,:]
+
+    x = np.delete(x, np.where(x==9999))
+    y = np.delete(y, np.where(y==9999))
+    z = np.delete(z, np.where(z==9999))
+
+
+    cov = np.cov(x,y)
+    lambda_, v = np.linalg.eig(cov)
+    lambda2 = np.sqrt(lambda_)
+
+
+    cov = np.cov(x,z)
+    lambda_, v = np.linalg.eig(cov)
+    lambdaz1 = np.sqrt(lambda_)
+
+    cov = np.cov(y,z)
+    lambda_, v = np.linalg.eig(cov)
+    lambdaz2 = np.sqrt(lambda_)
+    theta=np.rad2deg(np.arccos(v[0, 0]))
+
+
+    tempx = lambda2[0] * 111.11
+    tempy = lambda2[1] * 111.11
+    tempz = (lambdaz1[1] + lambdaz2[1])*0.5
+ 
+
+    ell = Ellipse(xy=(np.mean(y), np.mean(z)),
+                      width=lambda2[1]*1.96*2, height=tempz*1.96*2,
+                      angle=np.rad2deg(np.arccos(v[0, 0])))
+  
+   
+    ell.set_facecolor('none')
+    ell.set_edgecolor('b')
+    ell.set_linewidth(0.5)
+    ax.add_artist(ell)
+    ax.scatter(np.mean(y), np.mean(z), 0.1, 'r')
+
+
+   
+    ax.set_xlabel('Latitude [$^o$]')
+    ax.set_ylabel('Elevation [Km]')
+    cov = np.cov(y,z/111.11)
+    lambda_, v = np.linalg.eig(cov)
+    lambdaz1 = np.sqrt(lambda_)
+    theta=np.rad2deg(np.arccos(v[0, 0]))
+    #print(np.mean(y),np.mean(z),tempx,tempz,theta)
+
+    ll += 1
+
+fig.set_size_inches((10, 5), forward=False)
+fig.savefig('output/MapView_bootstrap1_NS.png', dpi=500) 
+fig.savefig('output/MapView_bootstrap1_NS.eps', dpi=500)
+
+plt.show()
---
+++ bootstrap_plot_palgunadi/plot_horizontal.py
@@ -0,0 +1,109 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+from mpl_toolkits.basemap import Basemap
+
+path = "G:\\My Drive\\Tomography\\040823\\bootstrp-result-sul-13042023-04082023\\"
+N = 50 #Number of Realization/Jumlah bootstrap sample
+M = 5000 #>= ID event terakhir
+hypo = np.loadtxt(path+'tomo0.reloc', usecols=(0,1,2,3))
+id = np.array([int(l)-1 for l in hypo[:,0]])
+hx = hypo[:,2]
+hy = hypo[:,1]
+hz = hypo[:,3]
+
+# initialize matrix
+X = np.zeros((M,N))
+Y = np.zeros((M,N))
+Z = np.zeros((M,N))
+
+for i in range(N):
+    data = np.loadtxt(path+'tomo'+str(i)+'.reloc', usecols=(0,1,2,3))
+    idx = np.array([int(l)-1 for l in data[:,0]])
+    X[idx,i] = data[:,2]
+    Y[idx,i] = data[:,1]
+    Z[idx,i] = data[:,3]
+
+X[X==0]=9999
+Y[Y==0]=9999
+Z[Z==0]=9999
+
+## MapView
+fig = plt.figure()
+ax = fig.add_subplot(111, aspect='equal')
+relative_error = open(path+'output/relative_error.txt', 'w+')
+relative_error.write('x_err [km]'+'\t'+'y_err [km]'+'\t'+'z_err [km]'+'\n')
+
+ll = 0
+for mm in id:
+    x = X[mm,:]
+    y = Y[mm,:]
+    z = Z[mm,:]
+
+    x = np.delete(x, np.where(x==9999))
+    y = np.delete(y, np.where(y==9999))
+    z = np.delete(z, np.where(z==9999))
+
+
+    cov = np.cov(x,z)
+    lambda_, v = np.linalg.eig(cov)
+    lambdaz1 = np.sqrt(lambda_)
+
+    cov = np.cov(y,z)
+    lambda_, v = np.linalg.eig(cov)
+    lambdaz2 = np.sqrt(lambda_)
+
+    cov = np.cov(x,y)
+    lambda_, v = np.linalg.eig(cov)
+    lambda2 = np.sqrt(lambda_)
+    theta =np.rad2deg(np.arccos(v[0, 0]))
+   
+    tempx = lambda2[0] * 111.11
+    tempy = lambda2[1] * 111.11
+    tempz = (lambdaz1[1] + lambdaz2[1])*0.5
+
+    relative_error.write(str(tempx)+'\t'+str(tempy)+'\t'+str(tempz)+'\n')
+
+    ax.scatter(np.mean(x), np.mean(y), 0.5, edgecolors="blue", facecolor=None)
+    ell = Ellipse(xy=(np.mean(x), np.mean(y)),
+                      width=lambda2[0]*1.96*2, height=lambda2[1]*1.96*2,
+                      angle=theta)
+
+    ell.set_facecolor('none')
+    ell.set_edgecolor('b')
+    ell.set_linewidth(0.5)
+    ax.add_artist(ell)
+
+
+    #print(np.mean(x), np.mean(y),tempx,tempy,theta)
+    lebar=lambda2[0]*1.96*2*111.11
+    tinggi=lambda2[1]*1.96*2*111.11
+    #print(np.mean(x), np.mean(y),lebar,tinggi)
+
+    ll += 1
+
+
+relative_error.close()
+
+ax.set_xlabel('Lon.[$^o$]')
+ax.set_ylabel('Lat.[$^o$]')
+#tentukan batas koordinat peta
+lon_min=118.0
+lon_max=127.0
+lat_min=-7
+lat_max=5
+interval=0.5
+ax.set_xlim(lon_min, lon_max)
+ax.set_ylim(lat_min, lat_max)
+m = Basemap(llcrnrlat=lat_min,urcrnrlat=lat_max,\
+            llcrnrlon=lon_min,urcrnrlon=lon_max,lat_ts=interval,resolution='h', ax=ax)
+m.drawcoastlines(linewidth=0.5)
+m.fillcontinents(color='none',lake_color='none')
+m.drawmapboundary(fill_color='white')
+m.drawparallels(np.arange(lat_min,lat_max ,interval),labels=[1,0,0,0], linewidth=0.0)
+m.drawmeridians(np.arange(lon_min,lon_max),labels=[0,0,0,1], linewidth=0.0)
+# fig.savefig(path+'output/Top_View_bootstrap1.png', dpi=300, bbox_inches='tight')
+# fig.savefig('output/Top_View_bootstrap1.eps', dpi=2000, bbox_inches='tight')
+
+plt.show()
+
